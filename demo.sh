69cae9d6cfc6181b5918636eda232681f5642ba5
Ubuntu <ubuntu@ip-172-31-21-165.ec2.internal>
2026-02-12 15:49:17 +0000

    this contains only the basics of the shellscripting

---
+++ demo.sh
@@ -0,0 +1,30 @@
+
+#!/bin/sh
+
+
+##############
+
+#Author :vijay
+#
+#
+#Date : 12/02/2026
+#
+#
+#thie is the basic output for the disk space and processes
+#
+#
+#Version : v1
+
+
+
+set -x
+
+top
+
+df
+
+free
+
+
+mkdir shell1
+touch test.sh
